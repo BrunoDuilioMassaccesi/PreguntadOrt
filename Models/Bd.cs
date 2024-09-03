dd3cbdc16c6e7e8433d0f0fa02b248d1ab3a631f
BRUNO DUILIO MASSACCESI <48590334@ort>
2024-09-03 09:06:06 -0300

    .cs

--- Models/Bd.cs
+++ Models/Bd.cs
@@ -1,9 +1,71 @@
+using System;
 using System.Data.SqlClient;
 using Dapper;
 
 namespace PreguntadOrt;
-static public class Bd
+
+static public class BD
 {
-private static string _connectionString = @"Server=localhost; DataBase=PreguntadOrt; Trusted_Connection=True;";
+    private static string _connectionString = @"Server=localhost; DataBase=PreguntadOrt; Trusted_Connection=true;";
+
+    public static List<Categoria> ObtenerCategorias()
+    {
+        List<Categoria> Lista = null;
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Categoria>("SELECT * FROM Categorias").ToList();
+        }       
+        return Lista;
+    }
+
+    public static List<Dificultad> ObtenerDificultades()
+    {
+        List<Dificultad> Lista = null;
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Dificultad>("SELECT * FROM Dificultades").ToList();
+        }       
+        return Lista;
+    }
+
+    public static List<Pregunta> ObtenerPreguntas(int dificultad, int categoria)
+    {
+        List<Pregunta> Lista = null;
+        string sql = " ";
+        if(dificultad != -1 && categoria != -1)
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdDificultad = @pDificultad and IdCategoria = @pCategoria";
+        }
+        else if(dificultad == -1 && categoria == -1)
+        {
+            sql = "SELECT * FROM Preguntas";
+        }
+        else if(categoria == -1)
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdDificultad = @pDificultad";
+        }
+        else
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdCategoria = @pCategoria";
+        }
+
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Pregunta>(sql, new{pDificultad = dificultad, pCategoria = categoria}).ToList();
+        }       
+        return Lista;
+    }
+
+    public static List<Respuesta> ObtenerRespuestas(int idPregunta)
+    {
+        List<Respuesta> Lista = null;
+        string sql = "SELECT * FROM Respuestas WHERE IdPregunta = @pId";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Respuesta>(sql, new{pId = idPregunta}).ToList();
+        }       
+        return Lista;
+    }
+
 
 }
---
+++ Models/Juego.cs
@@ -0,0 +1,88 @@
+namespace PreguntadOrt;
+
+static public class Juego
+{
+    static private string Usuario;
+    static private int PuntajeActual;
+    static private int CantCorrectas;
+    static private int ContadorNroPreguntaActual;
+    static private Pregunta PreguntaActual;
+    static private List<Pregunta> ListaPreguntas;
+    static private List<Respuesta> ListaRespuestas;
+
+
+    static private void InicializarJuego(string user)
+    {
+        Usuario = user;
+        PuntajeActual = 0;
+        CantCorrectas = 0;
+        ContadorNroPreguntaActual = 0;
+        PreguntaActual = null;
+        ListaPreguntas = null; 
+        ListaRespuestas = null;
+    }
+
+    static public List<Categoria> ObtenerCategorias()
+    {
+        return(BD.ObtenerCategorias());
+    }
+
+    static public List<Dificultad> ObtenerDificultades()
+    {
+        return(BD.ObtenerDificultades());
+    }
+
+    static public void CargarPartida(string username, int dificultad, int categoria)
+    {
+        InicializarJuego(username);
+        ListaPreguntas = BD.ObtenerPreguntas(dificultad, categoria);
+    }
+
+    static public Pregunta ObtenerProximaPregunta()
+    {
+        PreguntaActual = ListaPreguntas[ContadorNroPreguntaActual];
+        return PreguntaActual;
+    }
+
+    static public List<Respuesta> ObtenerProximasRespuestas(int idPregunta)
+    {
+        ListaRespuestas = BD.ObtenerRespuestas(idPregunta);
+        return ListaRespuestas;
+    }
+
+    public static bool VerificarRespuesta (int IdRespuesta)
+    {
+        bool Respuesta = false;
+        foreach (Respuesta rta in ListaRespuestas)
+        {
+            if (rta.Correcta)
+            {
+                Respuesta = true;
+
+                    if(PreguntaActual.IdDificultad == 1)
+                    {
+                        PuntajeActual+=150;
+                    }
+                    else if(PreguntaActual.IdDificultad == 2)
+                    {
+                        PuntajeActual+=300;
+                    }
+                    else
+                    {
+                        PuntajeActual+=500;
+                    }
+                    CantCorrectas++;
+
+            }
+            
+        }        
+        return Respuesta;
+    }  
+
+
+
+    
+
+
+    
+}
