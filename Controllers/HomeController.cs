aa8e8c6a6f2a5ee0f715967837e7424212e0887f
TIAGO AGUSTIN GHELMAN <48245701@ort>
2024-09-05 13:35:05 -0300

    home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult ConfigurarJuego()
+    {
+        ViewBag.Bd.ObtenerCategorias;
+        ViewBag.Bd.ObtenerDificultades;
+        return View();
+    }
     public IActionResult Creditos()
     {
         return View();
